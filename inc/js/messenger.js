6eca067d79ce1db8f646355edcf01d16c9a96f43
Isaac Smith <isaacns98@gmail.com>
2018-02-26 13:27:39 -0500

    Added refresh listener

--- inc/js/messenger.js
+++ inc/js/messenger.js
@@ -4,14 +4,29 @@ $(document).ready( function()
         e.preventDefault();
         //test-conversation will be replaced with conversation id.
         $.ajax({
-            url: '/api/test-conversation',
+            url: '/api/test-conversation',  //TODO: change to conersation id
             method: 'post',
             data: {$('#text-entry-box'.val())}
         }).done(function(data) {
             if (data.message == 'Reply Successful')
-            socket.emit('new-message', 'test-conversation')
+                socket.emit('new-message', 'test-conversation') //TODO: change to conersation id
         }).always(function() {
             $('#text-entry-box'.val(''))
         });
     });
+
+//test-conversation will be replaced with conversation id.
+    socket.on('refresh', function(message) {
+        $.ajax({
+            url: '/api/test-conversation', //TODO: change to conersation id
+            method: 'get'
+        }).done(function(data) {
+            if (data.message == 'Reply Successful') {
+                $('#message-container').empty();
+                for (var i; i < data.obj.length; i++) {
+                    $("#message-container").append('<div class="card"> <div class="card-body">' + data.obj[i] + '</div> </div>');
+                }
+            }
+        })
+    })
 });
