d4337650676ab4a875e2c3c08ea7ba076443c88a
John Bland <johnbone23@gmail.com>
2018-03-05 13:48:16 -0500

    implement login and register logic

---
+++ inc/js/login.js
@@ -0,0 +1,29 @@
+$( document ).ready( function() {
+	console.log( 'ready' );
+
+	$( '#submit' ).on( 'click', function( e ) {
+		e.preventDefault();
+
+		if ( !validPassword( $( '#username' ).val() ) ) {
+			return;
+		}
+
+		if ( !validPassword( $( '#password' ).val() ) ) {
+			return;
+		}
+
+		var user = { username: $( '#username' ).val(), password: $( '#password' ).val() };
+
+		$.ajax({
+			url: '/auth/login',  //TODO: change to conersation id
+			method: 'post',
+			data: user
+		}).done(function(data) {
+			localStorage.setItem( 'token', data );
+			localStorage.setItem( 'userId', data );
+			console.log( data );
+		} ).fail( function( fqXHR, textStatus ) {
+			console.log( 'failure' );
+		} );
+	} );
+} );
--- inc/js/messenger.js
+++ inc/js/messenger.js
@@ -1,45 +1,45 @@
 $(document).ready( function() {
-    $.ajax({
-        url: '/api/test-conversation', //TODO: change to conersation id
-        method: 'get'
-    }).done(function(data) {
-        if (data.message == 'Reply Successful') {
-            $('#message-container').empty();
-            for (var i = 0; i < data.obj.length; i++) {
-                $("#message-container").append('<div class="card"> <div class="card-body">' + data.obj[i].text + '</div> </div>');
-            }
-        }
-    });
+	$.ajax({
+		url: '/api/test-conversation', //TODO: change to conersation id
+		method: 'get'
+	}).done(function(data) {
+		if (data.message == 'Reply Successful') {
+			$('#message-container').empty();
+			for (var i = 0; i < data.obj.length; i++) {
+				$("#message-container").append('<div class="card"> <div class="card-body">' + data.obj[i].text + '</div> </div>');
+			}
+		}
+	});
 
-    var socket = io();
-    $('#send-button').on('click', function(e) {
-        console.log( 'jeff' );
-        e.preventDefault();
-        //test-conversation will be replaced with conversation id.
-        $.ajax({
-            url: '/api/test-conversation',  //TODO: change to conersation id
-            method: 'post',
-            data: { text: $('#text-entry-box').val()}
-        }).done(function(data) {
-            if (data.message == 'Reply Successful')
-                socket.emit('new-message', 'test-conversation') //TODO: change to conersation id
-        }).always(function() {
-            $('#text-entry-box').val('');
-        });
-    });
+	var socket = io();
+	$('#send-button').on('click', function(e) {
+		console.log( 'jeff' );
+		e.preventDefault();
+		//test-conversation will be replaced with conversation id.
+		$.ajax({
+			url: '/api/test-conversation',  //TODO: change to conersation id
+			method: 'post',
+			data: { text: $('#text-entry-box').val()}
+		}).done(function(data) {
+			if (data.message == 'Reply Successful')
+				socket.emit('new-message', 'test-conversation') //TODO: change to conersation id
+		}).always(function() {
+			$('#text-entry-box').val('');
+		});
+	});
 
 //test-conversation will be replaced with conversation id.
-    socket.on('refresh', function(message) {
-        $.ajax({
-            url: '/api/test-conversation', //TODO: change to conersation id
-            method: 'get'
-        }).done(function(data) {
-            if (data.message == 'Reply Successful') {
-                $('#message-container').empty();
-                for (var i = 0; i < data.obj.length; i++) {
-                    $("#message-container").append('<div class="card"> <div class="card-body">' + data.obj[i].text + '</div> </div>');
-                }
-            }
-        });
-    });
+	socket.on('refresh', function(message) {
+		$.ajax({
+			url: '/api/test-conversation', //TODO: change to conersation id
+			method: 'get'
+		}).done(function(data) {
+			if (data.message == 'Reply Successful') {
+				$('#message-container').empty();
+				for (var i = 0; i < data.obj.length; i++) {
+					$("#message-container").append('<div class="card"> <div class="card-body">' + data.obj[i].text + '</div> </div>');
+				}
+			}
+		});
+	});
 });
