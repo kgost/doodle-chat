c1f3cb32ab1090806c1dd268f3cb5ff020bceed6
David Messick <dmessick512@gmail.com>
2018-03-19 13:54:13 -0400

    Added username and password checks

--- inc/js/register.js
+++ inc/js/register.js
@@ -1,4 +1,8 @@
 $( document ).ready( function() {
+	var username = false;
+	var password = false;
+	var cPassword = false;
+	
 	$( '#submit' ).on( 'click', function( e ) {
 		e.preventDefault();
 
@@ -18,6 +22,64 @@ $( document ).ready( function() {
 	} );
 	
 	$( '#username' ).on( 'keypress', function ( e ) {
-		$(this).val();
+		var uName = $(this).val();
+		$.ajax({
+			url: '/api/userUniqueness/' + uName,
+			method: 'get'
+		}).done(function(data) {
+			if (data.obj) {
+				username = true;
+			}
+			else {
+				username = false; //flash error
+			}
+			checkAll();
+		}).fail( function( fqXHR, textStatus ) {
+			if ( fqXHR.status == 401 ) {
+				socket.disconnect();
+				document.location.href='/login?e=401';
+			} else {
+				flashError( fqXHR.responseJSON.error.message );
+			}
+			checkAll();
+		});
 	});
+	
+	$( '#password' ).on( 'keypress', function ( e ) {
+		var pw = $(this).val();
+		var cpw = $( '#confirm-password' ).val();
+		if(pw.length >= 6 && pw == cpw) {
+			password = true;
+		}
+		else {
+			if(pw != cpw) { //flash not equal message
+				password = false;
+			}
+			else {	//less than 6 chars message
+				password = false;
+			}
+		}
+		checkAll();
+	});
+	
+	$( '#confirm-password' ).on( 'keypress', function ( e ) {
+		var cpw = $(this).val();
+		var pw = $( '#password' ).val();
+		if(pw == cpw) {
+			cPassword = true;
+		}
+		else { //flash not equal message
+			cPassword = false;
+		}
+		checkAll();
+	});
+	
+	function checkAll() {
+		if (username && password && cPassword) {
+			$( '#submit' ).prop('disabled', false);
+		}
+		else {
+			$( '#submit' ).prop('disabled', true);
+		}
+	}
 } );
