3f12c9ddd031dc8aee67bb7b33b3c108095658ca
David Messick <dmessick512@gmail.com>
2018-04-09 13:57:08 -0400

    Moved header styles to sass, fixed delete message button, fixed some styling.

--- inc/js/messenger.js
+++ inc/js/messenger.js
@@ -81,7 +81,7 @@ $(document).ready( function() {
 						showx += '<div id="' + data.obj[i]._id + '" class="card">';
 						showx += '<div class="card-body message">' + data.obj[i].text + '</div>';
 					if ( data.obj[i].user == localStorage.getItem('userId')) {
-						showx += 	'<i class="fas fa-pencil-alt edit-message"></i>';
+						showx += 	'<i class="fas fa-pencil-alt edit-message edit-button"></i>';
 						showx +=	'<button type="button" class="close delete-message" aria-label="Close"> <span aria-hidden="true">&times;</span></button>';
 					}
 					showx += '</div>';
@@ -226,6 +226,22 @@ $(document).ready( function() {
 		html +=	'</div>';
 		$(this).parent().append(html);
 	});
+	
+	/**
+	 * Delete Message Click Listener
+	 */
+	$('body').on('click', '.delete-message', function(e) {
+		$.ajax({
+			url: '/api/messages/' + $(this).parent().attr('id') + '?token=' + localStorage.getItem( 'token' ),
+			method: 'delete',
+			//Force users in conversation to update the covnersation
+		}).done(function(data) {
+			socket.emit('new-message', conversationId);
+			//Send fail
+		} ).fail( function( fqXHR, textStatus ) {
+			flashError( fqXHR.responseJSON.error.message );
+		} );
+	});
 
 	/**
 	 * Change Message Click Listener
@@ -298,7 +314,7 @@ $(document).ready( function() {
 						showx += '<div id="' + data.obj[i]._id + '" class="card">';
 						showx += '<div class="card-body message">' + data.obj[i].text + '</div>';
 					if ( data.obj[i].user == localStorage.getItem('userId')) {
-						showx += 	'<i class="fas fa-pencil-alt"></i>';
+						showx += 	'<i class="fas fa-pencil-alt edit-button"></i>';
 						showx +=	'<button type="button" class="close delete-message" aria-label="Close"> <span aria-hidden="true">&times;</span></button>';
 					}
 					showx += '</div>';
