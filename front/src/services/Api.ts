e232a76ed9d3e3cfe28908ac8ca74710e5c04807
John Bland <john.bland@24g.com>
2019-08-09 11:29:58 -0400

    use staging api

--- front/src/services/Api.ts
+++ front/src/services/Api.ts
@@ -3,11 +3,21 @@ import axios from 'axios';
 import store from '@/store';
 
 export default() => {
-  return axios.create({
-    baseURL: `${ location.protocol }//${ location.hostname }:8080/api`,
-    headers: {
-      'Authorization': store.state.token,
-      'Content-Type': 'application/json',
-    },
-  });
+  if ( location.hostname === 'staging.jackthelast.com' ) {
+    return axios.create({
+      baseURL: `${ location.protocol }//staging-api.jackthelast.com/api`,
+      headers: {
+        'Authorization': store.state.token,
+        'Content-Type': 'application/json',
+      },
+    });
+  } else {
+    return axios.create({
+      baseURL: `${ location.protocol }//${ location.hostname }:8080/api`,
+      headers: {
+        'Authorization': store.state.token,
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 };
